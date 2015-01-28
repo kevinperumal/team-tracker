44d22a34dd532f0f3646d282d89d2527c42745ff
Kevin Perumal <kevin.perumal@gmail.com>
2015-01-28 12:54:37 -0500

    add user edit functionality

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,11 +1,20 @@
 get '/user/:id' do |id|
-  @team = User.find(id).team
+  @user = User.find(id)
 
   erb :'user/show'
 end
 
 get '/user/:id/edit' do |id|
-  @team = User.find(id)
+  @user = User.find(id)
 
   erb :'user/edit'
+end
+
+put '/user/:id/edit' do |id|
+
+  @user = User.find(id)
+  @user.update(params[:user])
+
+  redirect "/user/#{id}"
+
 end
