afaba266b00cfc7707075f6d4f60d200e549de99
Kevin Perumal <kevin.perumal@gmail.com>
2015-01-28 11:54:40 -0500

    create signup routes and views

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,6 +7,34 @@ get '/login' do
   erb :login
 end
 
+post '/login' do
+  user = User.find_by(name: params[:user][:name])
+
+  if user.try(:authenticate, params[:user][:password])
+    session[:user_id] = user.id
+    redirect '/'
+
+  else
+    #need to add errors
+    redirect '/login'
+  end
+
+end
+
 get '/signup' do
+  erb: '/signup'
+end
+
+post '/signup' do
+  user = User.create(params[:user])
+
+  if user.save
+    session[:user_id] = user.id
+    redirect "/"
+  else
+
+    #need to add errors here
+    redirect "/login"
+  end
 
 end
