ffe8ebbe267186f4a64afc3e14ed65af6459e410
Kevin Perumal <kevin.perumal@gmail.com>
2015-01-28 16:41:03 -0500

    add method to return avaiable players

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,4 +4,12 @@ class Player < ActiveRecord::Base
 
   validates :name, presence: true
 
+  def self.get_available_players
+    self.where(team_id: nil)
+  end
+
+  def self.unavailable_players
+    self.where(team_id: !nil)
+  end
+
 end
